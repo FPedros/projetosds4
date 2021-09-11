c6a48d13b7209ca884e0d5c112ed350151a20771
Fernando <fernandoppsousa@gmail.com>
2021-09-11 10:21:04 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-      <div>
+    <>
+      <NavBar />
+      <div className="conteiner">
         <h1 className="text-primary"> Hellooo</h1>
+      
+      <DataTable />
       </div>
+      <Footer />
+    </>
   );
 }
 
